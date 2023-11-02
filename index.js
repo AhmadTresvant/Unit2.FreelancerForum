b2af146a1a0f35a98b7c1930d8b9fe91682e8b00
Ahmad Ali Tresvant <ahmadtresvant@Alis-M1-Pro.local>
2023-11-02 16:21:51 -0400

    made working function that gives average price of freelancers

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+const freelancers = [
+  { name: "Alice", price: 30, occupation: "writer" },
+  { name: "Bob", price: 50, occupation: "teacher" },
+  { name: "Carol", price: 70, occupation: "programmer" },
+];
+
+const findAverage = () => {
+const sum = freelancers.reduce((total, item) => total + item.price, 0);
+return sum / freelancers.length;
+}
+
+console.log(findAverage());
+
+const freelanceTitle = document.createElement("h1");
+const averageStartingPrice = document.createElement("h3");
+const availableFreeLance = document.createElement("h2");
+const name = document.createElement("h3");
+const occupation = document.createElement("h3");
+const startingPrice = document.createElement("h3");
+const nameUl = document.createElement("ul");
+const occupationUl = document.createElement("ul");
+const startingPriceUl = document.createElement("ul");
+
+
+
 
